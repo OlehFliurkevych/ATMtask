6459433745a574d3e20f02dd1537c27511e47f16
OlehFliurkevych <oleh0809@gmail.com>
2018-09-12 19:34:10 +0300

    Add search withdrawal by year,month,day

--- src/main/java/fo/test/ATM/controller/WithdrawalController.java
+++ src/main/java/fo/test/ATM/controller/WithdrawalController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,29 @@ public class WithdrawalController {
 		return withdrawalService.findWithdrawalById(withdrawalId);
 	}
 	
+	@RequestMapping(value="/withdrawals/user/{id}/by_year",method=RequestMethod.GET)
+	@ResponseBody
+	public List<WithdrawalDTO> getIncomesForUserForYear(
+			@PathVariable("id")Long userId,
+			@RequestParam(name="year",required=false)int year){
+		return withdrawalService.findAllWithdrawalByYear(userId, year);
+	}
 	
+	@RequestMapping(value="/withdrawals/user/{id}/by_month",method=RequestMethod.GET)
+	@ResponseBody
+	public List<WithdrawalDTO> getIncomesForUserForMonth(
+			@PathVariable("id")Long userId,
+			@RequestParam(name="month",required=false)int month){
+		return withdrawalService.findAllWithdrawalByMonth(userId, month);
+	}
+	
+	@RequestMapping(value="/withdrawals/user/{id}/by_day",method=RequestMethod.GET)
+	@ResponseBody
+	public List<WithdrawalDTO> getIncomesForUserForDay(
+			@PathVariable("id")Long userId,
+			@RequestParam(name="day",required=false)int day){
+		return withdrawalService.findAllWithdrawalByDay(userId, day);
+	}
 	
 	
 	
